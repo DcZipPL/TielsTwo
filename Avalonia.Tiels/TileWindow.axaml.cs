c0796d1024b5cb7f977959631228928dcc10c39b
Kordian Kisieliński <dczip89@gmail.com>
2022-11-21 01:40:16 +0100

    Add all project files

---
+++ Avalonia.Tiels/TileWindow.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.Tiels;
+
+public partial class TileWindow : Window
+{
+	public TileWindow()
+	{
+		InitializeComponent();
+#if DEBUG
+		this.AttachDevTools();
+#endif
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}

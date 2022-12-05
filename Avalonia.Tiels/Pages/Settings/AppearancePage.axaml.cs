0289540d2a96c9a6c38a21fd84cd949bd2f05b0c
Kordian Kisieliński <dczip89@gmail.com>
2022-12-06 00:35:04 +0100

    Add color selection in Appearance Page

--- Avalonia.Tiels/Pages/Settings/AppearancePage.axaml.cs
+++ Avalonia.Tiels/Pages/Settings/AppearancePage.axaml.cs
@@ -1,5 +1,11 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace Avalonia.Tiels.Pages.Settings;
 
@@ -8,5 +14,14 @@ public partial class AppearancePage : UserPage
 	public AppearancePage()
 	{
 		InitializeComponent();
+	}
+	
+	private void ColorWheelSizeChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+	{
+		if (ColorWheel != null)
+		{
+			ColorWheel.IsHSBVisible = ColorWheel.Bounds.Width > 890;
+			ColorWheel.IsRGBVisible = ColorWheel.Bounds.Width > 890;
+		}
 	}
 }
